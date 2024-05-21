7a4c40dc9654bdba388f22039bae54d075c4dd73
Orestes Za <37294610+orestesza@users.noreply.github.com>
2024-05-21 21:32:50 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   socialLink: '',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
-    #googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
